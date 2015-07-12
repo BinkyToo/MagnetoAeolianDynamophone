c8aad426e026d83c008f89de616efa908cee91d0
Christopher Stone <cedstone@lithoi.org.uk>
2015-07-12 20:41:12 +0100

    added heading comments to zenity script

--- zenitycontrolscript.sh
+++ zenitycontrolscript.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This script provites a simple gui interface for triggering a hardware drum-beater over a (virtual) serial port
+# Originally written to go with an Arduino running "MADdrumslave"
 
 serialport=$(zenity --entry --title "Enter path to serial port"--text "Which serial port is your tambourine thumper connected to?" --entry-text "/dev/ttyACM0"); echo $serialport
 if test -z "$serialport" ; then
