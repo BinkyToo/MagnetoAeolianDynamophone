baae0b7aaf1e54eb67023841aeed082bfa75ebe0
Christopher Stone <cedstone@lithoi.org.uk>
2015-07-12 20:43:26 +0100

    added inline comments to zenity script

--- zenitycontrolscript.sh
+++ zenitycontrolscript.sh
@@ -3,17 +3,17 @@
 # Originally written to go with an Arduino running "MADdrumslave"
 
 serialport=$(zenity --entry --title "Enter path to serial port"--text "Which serial port is your tambourine thumper connected to?" --entry-text "/dev/ttyACM0"); echo $serialport
-if test -z "$serialport" ; then
+if test -z "$serialport" ; then #Quit if the user pressed cancel
     exit 1
 fi
 thumps=$(zenity --scale --text "How many thumps" --min-value=1 --max-value=20 --value=3 --step 1)
-if test -z "$thumps" ; then
+if test -z "$thumps" ; then     #Quit if the user pressed cancel
     exit 2
 fi
 while (zenity --question --text="Thump tambourine $thumps times?") do
     echo $thumps
-    for i in $(seq 1 $thumps) ; do
-        echo "drums:*," > $serialport
-        sleep 0.2
+    for i in $(seq 1 $thumps) ; do  #repeatedly trigger a drum thump
+        echo "drums:*," > $serialport   #send command string over serial connection
+        sleep 0.2                       #FIXME this shouldn't be necessary
     done
 done
