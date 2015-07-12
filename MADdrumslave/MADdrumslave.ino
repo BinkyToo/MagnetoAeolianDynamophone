d10aa7bfbd76519948966409cbe39352a227a324
Christopher Stone <cedstone@lithoi.org.uk>
2015-07-12 20:50:40 +0100

    Added heading comment for drum slave arduino code

--- MADdrumslave/MADdrumslave.ino
+++ MADdrumslave/MADdrumslave.ino
@@ -1,3 +1,8 @@
+/* This programme runs on a slave controller in an automated musical instrument.
+ *  Command strings are off the format address:note,duration
+ *  In this particualr case, duration isn't used
+ */
+
 int drumpin = 13;
 const int drumdelay = 45;
 
