182e9d6346c2bc251af9d4361de1d653ae73aa02
Christopher Stone <cedstone@lithoi.org.uk>
2015-07-13 17:11:06 +0100

    tidied up window titles etc

--- zenitycontrolscript.sh
+++ zenitycontrolscript.sh
@@ -2,15 +2,15 @@
 # This script provites a simple gui interface for triggering a hardware drum-beater over a (virtual) serial port
 # Originally written to go with an Arduino running "MADdrumslave"
 
-serialport=$(zenity --entry --title "Enter path to serial port"--text "Which serial port is your tambourine thumper connected to?" --entry-text "/dev/ttyACM0"); echo $serialport
+serialport=$(zenity --entry --title "Tambourine thumper control"--text "Which serial port is your tambourine thumper connected to?" --entry-text "/dev/ttyACM0"); echo $serialport
 if test -z "$serialport" ; then #Quit if the user pressed cancel
     exit 1
 fi
-thumps=$(zenity --scale --text "How many thumps" --min-value=1 --max-value=20 --value=3 --step 1)
+thumps=$(zenity --scale --title "Tambourine thumper control" --text "How many thumps?" --min-value=1 --max-value=20 --value=3 --step 1)
 if test -z "$thumps" ; then     #Quit if the user pressed cancel
     exit 2
 fi
-while (zenity --question --text="Thump tambourine $thumps times?") do
+while (zenity --question --title "Tambourine thumper control" --text="Thump tambourine $thumps times?") do
     for i in $(seq 1 $thumps) ; do  #repeatedly trigger a drum thump
         echo "percussion:*," > $serialport   #send command string over serial connection
         if [ $? = 1 ] ; then
