8c51b803095c9bc4bbac7b2eed1317a8b148be82
Christopher Stone <cedstone@lithoi.org.uk>
2015-07-20 21:58:56 +0100

    Added lots of comments, and did a little tidying

--- MADstepperslave/MADstepperslave.ino
+++ MADstepperslave/MADstepperslave.ino
@@ -1,21 +1,20 @@
-int step1 = 2;
+int step1 = 2;            // The pins used to control the stepper motor
 int step2 = 3;
 int step3 = 4;
 int step4 = 5;
 
-const String address = "stepper";
-String addressstring = "";
+const String address = "stepper";   // Address used for recognising incoming commands
+String addressstring = "";          // Initialise strings which will be populated by processing incomign commands
 String pitchstring = "";
 String timestring = "";
 
-int bpm = 180;
-int pause = 0;
-long cycles = 0;
+int bpm = 180;                      // Playback speed FIXME should be used in slave device
+long cycles = 0;                    // Counting how many complete stepper rotations have been done
 long cyclesDone = 0;
-const int highTonedurations[] = {3822, 3405, 3034, 2703, 2551, 2273, 2025};
+const int highTonedurations[] = {3822, 3405, 3034, 2703, 2551, 2273, 2025};   // Wave period in microseconds
 const int lowTonedurations[] = {7644, 6810, 6060, 5406, 5102, 4546, 4050};
-long duration = 30000000 / bpm;  //calculates the duration in microseconds for each quaver
-boolean forwards = 0;
+long duration = 30000000 / bpm;  //calculates the duration in microseconds for each quaver    // Placeholder initialisation
+boolean forwards = 0;               // Which way is the stepper turning? we keep reversing it.
 
 void setup() {
   Serial.begin(9600);
@@ -35,16 +34,16 @@ void loop() {
     timestring = Serial.readStringUntil('\n');
     if (addressstring == address) {
       Serial.println("\tData is addressed to this module");
-      if (timestring.length() == 1) {
-        int foo = 1*timestring.charAt(0)-48;
+      if (timestring.length() == 1) {         // FIXME should be able to handle multi-digit numbers
+        int foo = 1*timestring.charAt(0)-48;  // debug stuff to be removed
         Serial.println(foo);
         Serial.println(bpm);
-        duration = (30000000 / bpm) * (foo);
+        duration = (30000000 / bpm) * (foo);  // How long the note is played for
         Serial.print("Setting duration to "); Serial.println(duration);
       }
       if (pitchstring.length() == 1) {
         Serial.println("Seems to have a valid single-character pitch string");
-        byte ch = pitchstring.charAt(0);
+        byte ch = pitchstring.charAt(0);     // This bit can almost certainly be simplified (FIXME)
         if (ch >= 'C' and ch <= 'G') {
           playNote(lowTonedurations[ch - 'C']);
         }
@@ -85,7 +84,7 @@ void playNote(long timePeriod) {
   Serial.print(cycles); Serial.println(" cycles");
   timePeriod = timePeriod - 20;    //it takes 20 us to do the lines.
 
-  digitalWrite(step1, HIGH);
+  digitalWrite(step1, HIGH);       // why the extra cycle? DNRY seems aplicable here FIXME
   delayMicroseconds(timePeriod);
   digitalWrite(step1, LOW);
 
