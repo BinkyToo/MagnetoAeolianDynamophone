7cfd79bd66a16eba94609f9b5aabd1697e6ded60
Christopher Stone <cedstone@lithoi.org.uk>
2015-07-20 23:18:51 +0100

    Moved some LCD management functions into separate file

--- MADmaster/MADmaster.ino
+++ MADmaster/MADmaster.ino
@@ -111,22 +111,7 @@ void scansdcard(){
    }
 }
 
-void movelcdcursortochar(int place){
-  Serial1.write(0x02);                    // Next byte is a screen position
-  Serial1.write(1+place);                 // FIXME are we using 1 or 0 indexing?
-}
 
-void movelcdcursortoline(int line){       // Pretty much like above
-  Serial1.write(0x02);
-  Serial1.write(1+(line*20));
-}
-
-void lcdsetup(){
-  Serial1.begin(9600,SERIAL_8N2);  // two stop bits (not default)
-  Serial1.write(0x13);             // Turn backlight on
-  Serial1.write(0x04);             // Turn cursor off
-  Serial1.print('\f');             // Form feed (return to top left?)
-}
 
 void tryuistatechange(){
   if (uistate != olduistate){
