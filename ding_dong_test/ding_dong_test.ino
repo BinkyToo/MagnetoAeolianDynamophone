36e688830d6b761f36a54ce714ce906315cec66a
Thomas <t.potato2@gmail.com>
2015-08-19 13:06:25 +0100

    Circuit diagrams in png form and a ding dong test for the bell driving circuit that now works.

---
+++ ding_dong_test/ding_dong_test.ino
@@ -0,0 +1,21 @@
+int ding = 2;  //possibly dong
+int dong = 3;  //possibly ding
+int dingTime = 40; //38 seems to be the minimum time to get a ding but will be quieter
+int space = 1000;
+void setup() 
+{
+pinMode(ding,OUTPUT);
+pinMode(dong,OUTPUT);
+}
+
+void loop() 
+{
+digitalWrite(ding,HIGH); //Does this really need a comment?
+delay(dingTime);
+digitalWrite(ding,LOW);
+delay(space-dingTime);
+digitalWrite(dong,HIGH);
+delay(dingTime);
+digitalWrite(dong,LOW);
+delay(space-dingTime);
+}
